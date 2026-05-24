c57495fd4d0edd3c194eed259ab5f00c3d020ae3
janoslovasz1986 <janos.l0vasz86@yahoo.com>
2026-05-24 22:02:41 +0200

    warmup Settingspage

--- PdfMergerApp/AppShell.xaml.cs
+++ PdfMergerApp/AppShell.xaml.cs
@@ -5,6 +5,22 @@
         public AppShell()
         {
             InitializeComponent();
+            PreloadSettingsPage();
+        }
+
+        private void PreloadSettingsPage()
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(1500); // app indulás után
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    // Briefly switch to Settings tab and back
+                    var mainTab = CurrentItem;
+                    CurrentItem = Items[0].Items[1]; // Settings tab
+                    CurrentItem = mainTab; // vissza
+                });
+            });
         }
     }
 }
